f13120a27ca0c39d24ad50a247a9ae93f8a4df22
Boban Talevski <talevski.boban@gmail.com>
2017-12-12 11:30:47 +0100

    end of map examples

--- src/main/java/com/teamtreehouse/jobs/App.java
+++ src/main/java/com/teamtreehouse/jobs/App.java
@@ -6,44 +6,45 @@ import com.teamtreehouse.jobs.service.JobService;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class App {
 
-  public static void main(String[] args) {
-    JobService service = new JobService();
-    boolean shouldRefresh = false;
-    try {
-      if (shouldRefresh) {
-        service.refresh();
-      }
-      List<Job> jobs = service.loadJobs();
-      System.out.printf("Total jobs:  %d %n %n", jobs.size());
-      explore(jobs);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        JobService service = new JobService();
+        boolean shouldRefresh = false;
+        try {
+            if (shouldRefresh) {
+                service.refresh();
+            }
+            List<Job> jobs = service.loadJobs();
+            System.out.printf("Total jobs:  %d %n %n", jobs.size());
+            explore(jobs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private static void explore(List<Job> jobs) {
-    // Your amazing code below...
-    getThreeJuniorJobsStream(jobs).forEach(System.out::println);
-  }
+    private static void explore(List<Job> jobs) {
+        // Your amazing code below...
+        getCaptionsStream(jobs).forEach(System.out::println);
+    }
 
-  private static boolean isJuniorJob(Job job) {
-    String title = job.getTitle().toLowerCase();
-    return title.contains("junior") || title.contains("jr");
-  }
+    private static boolean isJuniorJob(Job job) {
+        String title = job.getTitle().toLowerCase();
+        return title.contains("junior") || title.contains("jr");
+    }
 
-  // "Senior Dev", "Jr. Java Engineer", "Java Evangelist", "Junior Java Dev",
-  // "Sr. Java Wizard Ninja", "Junior Java Wizard Ninja", "Full Stack Java Engineer"
-  private static List<Job> getThreeJuniorJobsStream(List<Job> jobs) {
-    return jobs.stream()
-            .filter(App::isJuniorJob)
-            .limit(3) //stateful short circuiting intermediate operation
-            .collect(Collectors.toList());
+    // "Senior Dev", "Jr. Java Engineer", "Java Evangelist", "Junior Java Dev",
+    // "Sr. Java Wizard Ninja", "Junior Java Wizard Ninja", "Full Stack Java Engineer"
+    private static List<Job> getThreeJuniorJobsStream(List<Job> jobs) {
+        return jobs.stream()
+                .filter(App::isJuniorJob)
+                .limit(3) //stateful short circuiting intermediate operation
+                .collect(Collectors.toList());
 
-    // Another option. This is an example of a method relying on a side effect
+        // Another option. This is an example of a method relying on a side effect
 //    List<Job> juniorJobs = new ArrayList<>();
 //    jobs.stream()
 //            .filter(App::isJuniorJob)
@@ -53,33 +54,59 @@ public class App {
 //            // although it works, it's bad form, because it could be solved
 //            // without side effects
 //    return juniorJobs;
-  }
+    }
 
-  private static List<Job> getThreeJuniorJobsInmperatively(List<Job> jobs) {
-    List<Job> juniorJobs = new ArrayList<>();
-    for (Job job : jobs) {
-      if (isJuniorJob(job)) {
-        juniorJobs.add(job);
-        if (juniorJobs.size() >= 3) {
-          break;
+    private static List<Job> getThreeJuniorJobsInmperatively(List<Job> jobs) {
+        List<Job> juniorJobs = new ArrayList<>();
+        for (Job job : jobs) {
+            if (isJuniorJob(job)) {
+                juniorJobs.add(job);
+                if (juniorJobs.size() >= 3) {
+                    break;
+                }
+            }
         }
-      }
+        return juniorJobs;
     }
-    return juniorJobs;
-  }
 
-  private static void printPortlandJobsImperatively(List<Job> jobs) {
-    for (Job job : jobs) {
-      if (job.getState().equals("OR") && job.getCity().equals("Portland")) {
-        System.out.println(job);
-      }
+    private static List<String> getCaptionsStream(List<Job> jobs) {
+        return jobs.stream()
+                .filter(App::isJuniorJob) // this is static method
+                .map(Job::getCaption) // this is instance method
+                // syntax is the same, because in the case of the instance method
+                // it's assumed that the instance on which the method is called
+                // will be a parameter (the job object from the stream)
+                // This is -Method reference (type) inference-
+                .limit(3) //stateful short circuiting intermediate operation
+                .collect(Collectors.toList());
+
     }
-  }
 
-  private static void printPortlandJobsStream(List<Job> jobs) {
-    jobs.stream()
-        .filter(job -> job.getState().equals("OR"))
-        .filter(job -> job.getCity().equals("Portland"))
-        .forEach(System.out::println);
-  }
+    private static List<String> getCaptionsImperatively(List<Job> jobs) {
+        List<String> captions = new ArrayList<>();
+        for (Job job : jobs) {
+            if (isJuniorJob(job)) {
+                captions.add(job.getCaption());
+                if (captions.size() >= 3) {
+                    break;
+                }
+            }
+        }
+        return captions;
+    }
+
+    private static void printPortlandJobsImperatively(List<Job> jobs) {
+        for (Job job : jobs) {
+            if (job.getState().equals("OR") && job.getCity().equals("Portland")) {
+                System.out.println(job);
+            }
+        }
+    }
+
+    private static void printPortlandJobsStream(List<Job> jobs) {
+        jobs.stream()
+                .filter(job -> job.getState().equals("OR"))
+                .filter(job -> job.getCity().equals("Portland"))
+                .forEach(System.out::println);
+    }
 }
--- src/main/java/com/teamtreehouse/jobs/model/Job.java
+++ src/main/java/com/teamtreehouse/jobs/model/Job.java
@@ -3,97 +3,103 @@ package com.teamtreehouse.jobs.model;
 import com.google.api.client.util.Key;
 
 public class Job {
-  @Key
-  private String jobtitle;
+    @Key
+    private String jobtitle;
 
-  @Key
-  private String company;
-
-
-  @Key
-  private String city;
+    @Key
+    private String company;
 
 
-  @Key
-  private String state;
+    @Key
+    private String city;
 
 
-  @Key
-  private String country;
+    @Key
+    private String state;
 
 
-  @Key
-  private String snippet;
-
-  @Key
-  private String date;
-
-  public String getDateTimeString() {
-    return date;
-  }
+    @Key
+    private String country;
 
 
+    @Key
+    private String snippet;
 
-  public String getTitle() {
-    return jobtitle;
-  }
+    @Key
+    private String date;
 
-  public String getCompany() {
-    return company;
-  }
+    public String getDateTimeString() {
+        return date;
+    }
 
-  public String getCity() {
-    return city;
-  }
 
-  public String getState() {
-    return state;
-  }
+    public String getTitle() {
+        return jobtitle;
+    }
 
-  public String getCountry() {
-    return country;
-  }
+    public String getCompany() {
+        return company;
+    }
 
-  public String getSnippet() {
-    return snippet;
-  }
+    public String getCity() {
+        return city;
+    }
 
-  @Override
-  public String toString() {
-    return "Job{" +
-            "title='" + jobtitle + '\'' +
-            ", company='" + company + '\'' +
-            ", city='" + city + '\'' +
-            ", state='" + state + '\'' +
-            ", country='" + country + '\'' +
-            '}';
-  }
+    public String getState() {
+        return state;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Job)) return false;
+    public String getCountry() {
+        return country;
+    }
 
-    Job job = (Job) o;
+    public String getSnippet() {
+        return snippet;
+    }
 
-    if (!jobtitle.equals(job.jobtitle)) return false;
-    if (!company.equals(job.company)) return false;
-    if (city != null ? !city.equals(job.city) : job.city != null) return false;
-    if (state != null ? !state.equals(job.state) : job.state != null) return false;
-    if (country != null ? !country.equals(job.country) : job.country != null) return false;
-    if (snippet != null ? !snippet.equals(job.snippet) : job.snippet != null) return false;
-    return date != null ? date.equals(job.date) : job.date == null;
-  }
+    @Override
+    public String toString() {
+        return "Job{" +
+                "title='" + jobtitle + '\'' +
+                ", company='" + company + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 
-  @Override
-  public int hashCode() {
-    int result = jobtitle.hashCode();
-    result = 31 * result + company.hashCode();
-    result = 31 * result + (city != null ? city.hashCode() : 0);
-    result = 31 * result + (state != null ? state.hashCode() : 0);
-    result = 31 * result + (country != null ? country.hashCode() : 0);
-    result = 31 * result + (snippet != null ? snippet.hashCode() : 0);
-    result = 31 * result + (date != null ? date.hashCode() : 0);
-    return result;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+
+        Job job = (Job) o;
+
+        if (!jobtitle.equals(job.jobtitle)) return false;
+        if (!company.equals(job.company)) return false;
+        if (city != null ? !city.equals(job.city) : job.city != null) return false;
+        if (state != null ? !state.equals(job.state) : job.state != null) return false;
+        if (country != null ? !country.equals(job.country) : job.country != null) return false;
+        if (snippet != null ? !snippet.equals(job.snippet) : job.snippet != null) return false;
+        return date != null ? date.equals(job.date) : job.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = jobtitle.hashCode();
+        result = 31 * result + company.hashCode();
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (snippet != null ? snippet.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+
+    public String getCaption() {
+        return String.format("%s is looking for a %s in %s",
+                getCompany(),
+                getTitle(),
+                getCity());
+    }
 }
